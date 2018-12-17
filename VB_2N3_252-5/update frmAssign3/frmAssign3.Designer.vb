83d9ac1b9c5cf4ae3ff28f4e6479f94be9e1a90b
252-5 <bigbang12371@gmail.com>
2018-12-17 10:53:33 +0700

    update frmAssign3
    อัพเดท Lab 3 (17 /12/61)

---
+++ VB_2N3_252-5/update frmAssign3/frmAssign3.Designer.vb
@@ -0,0 +1,365 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAssign3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblFSky = New System.Windows.Forms.Label()
+        Me.lblFWhite = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblBSky = New System.Windows.Forms.Label()
+        Me.lblBWhite = New System.Windows.Forms.Label()
+        Me.lblBBlack = New System.Windows.Forms.Label()
+        Me.lblBBrown = New System.Windows.Forms.Label()
+        Me.lblBViolet = New System.Windows.Forms.Label()
+        Me.lblBYellow = New System.Windows.Forms.Label()
+        Me.lblBGreen = New System.Windows.Forms.Label()
+        Me.lblBOrange = New System.Windows.Forms.Label()
+        Me.lblBBlue = New System.Windows.Forms.Label()
+        Me.lblBRed = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblFBlack = New System.Windows.Forms.Label()
+        Me.lblFBrown = New System.Windows.Forms.Label()
+        Me.lblFViolet = New System.Windows.Forms.Label()
+        Me.lblFYellow = New System.Windows.Forms.Label()
+        Me.lblFGreen = New System.Windows.Forms.Label()
+        Me.lblFOrange = New System.Windows.Forms.Label()
+        Me.lblFBlue = New System.Windows.Forms.Label()
+        Me.lblFRed = New System.Windows.Forms.Label()
+        Me.cboSelect = New System.Windows.Forms.ComboBox()
+        Me.btnLow = New System.Windows.Forms.Button()
+        Me.btnHigh = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAdd = New System.Windows.Forms.TextBox()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblFSky
+        '
+        Me.lblFSky.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblFSky.Location = New System.Drawing.Point(200, 67)
+        Me.lblFSky.Name = "lblFSky"
+        Me.lblFSky.Size = New System.Drawing.Size(40, 32)
+        Me.lblFSky.TabIndex = 0
+        '
+        'lblFWhite
+        '
+        Me.lblFWhite.BackColor = System.Drawing.Color.White
+        Me.lblFWhite.Location = New System.Drawing.Point(154, 67)
+        Me.lblFWhite.Name = "lblFWhite"
+        Me.lblFWhite.Size = New System.Drawing.Size(40, 32)
+        Me.lblFWhite.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.SpringGreen
+        Me.GroupBox2.Controls.Add(Me.lblBSky)
+        Me.GroupBox2.Controls.Add(Me.lblBWhite)
+        Me.GroupBox2.Controls.Add(Me.lblBBlack)
+        Me.GroupBox2.Controls.Add(Me.lblBBrown)
+        Me.GroupBox2.Controls.Add(Me.lblBViolet)
+        Me.GroupBox2.Controls.Add(Me.lblBYellow)
+        Me.GroupBox2.Controls.Add(Me.lblBGreen)
+        Me.GroupBox2.Controls.Add(Me.lblBOrange)
+        Me.GroupBox2.Controls.Add(Me.lblBBlue)
+        Me.GroupBox2.Controls.Add(Me.lblBRed)
+        Me.GroupBox2.Location = New System.Drawing.Point(385, 221)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(264, 123)
+        Me.GroupBox2.TabIndex = 19
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "BgColor"
+        '
+        'lblBSky
+        '
+        Me.lblBSky.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblBSky.Location = New System.Drawing.Point(200, 67)
+        Me.lblBSky.Name = "lblBSky"
+        Me.lblBSky.Size = New System.Drawing.Size(40, 32)
+        Me.lblBSky.TabIndex = 0
+        '
+        'lblBWhite
+        '
+        Me.lblBWhite.BackColor = System.Drawing.Color.White
+        Me.lblBWhite.Location = New System.Drawing.Point(154, 67)
+        Me.lblBWhite.Name = "lblBWhite"
+        Me.lblBWhite.Size = New System.Drawing.Size(40, 32)
+        Me.lblBWhite.TabIndex = 0
+        '
+        'lblBBlack
+        '
+        Me.lblBBlack.BackColor = System.Drawing.Color.Black
+        Me.lblBBlack.Location = New System.Drawing.Point(200, 26)
+        Me.lblBBlack.Name = "lblBBlack"
+        Me.lblBBlack.Size = New System.Drawing.Size(40, 32)
+        Me.lblBBlack.TabIndex = 0
+        '
+        'lblBBrown
+        '
+        Me.lblBBrown.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblBBrown.Location = New System.Drawing.Point(108, 67)
+        Me.lblBBrown.Name = "lblBBrown"
+        Me.lblBBrown.Size = New System.Drawing.Size(40, 32)
+        Me.lblBBrown.TabIndex = 0
+        '
+        'lblBViolet
+        '
+        Me.lblBViolet.BackColor = System.Drawing.Color.Fuchsia
+        Me.lblBViolet.Location = New System.Drawing.Point(154, 26)
+        Me.lblBViolet.Name = "lblBViolet"
+        Me.lblBViolet.Size = New System.Drawing.Size(40, 32)
+        Me.lblBViolet.TabIndex = 0
+        '
+        'lblBYellow
+        '
+        Me.lblBYellow.BackColor = System.Drawing.Color.Yellow
+        Me.lblBYellow.Location = New System.Drawing.Point(62, 67)
+        Me.lblBYellow.Name = "lblBYellow"
+        Me.lblBYellow.Size = New System.Drawing.Size(40, 32)
+        Me.lblBYellow.TabIndex = 0
+        '
+        'lblBGreen
+        '
+        Me.lblBGreen.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblBGreen.Location = New System.Drawing.Point(108, 26)
+        Me.lblBGreen.Name = "lblBGreen"
+        Me.lblBGreen.Size = New System.Drawing.Size(40, 32)
+        Me.lblBGreen.TabIndex = 0
+        '
+        'lblBOrange
+        '
+        Me.lblBOrange.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblBOrange.Location = New System.Drawing.Point(16, 67)
+        Me.lblBOrange.Name = "lblBOrange"
+        Me.lblBOrange.Size = New System.Drawing.Size(40, 32)
+        Me.lblBOrange.TabIndex = 0
+        '
+        'lblBBlue
+        '
+        Me.lblBBlue.BackColor = System.Drawing.Color.Blue
+        Me.lblBBlue.Location = New System.Drawing.Point(62, 26)
+        Me.lblBBlue.Name = "lblBBlue"
+        Me.lblBBlue.Size = New System.Drawing.Size(40, 32)
+        Me.lblBBlue.TabIndex = 0
+        '
+        'lblBRed
+        '
+        Me.lblBRed.BackColor = System.Drawing.Color.Red
+        Me.lblBRed.Location = New System.Drawing.Point(16, 26)
+        Me.lblBRed.Name = "lblBRed"
+        Me.lblBRed.Size = New System.Drawing.Size(40, 32)
+        Me.lblBRed.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.NavajoWhite
+        Me.GroupBox1.Controls.Add(Me.lblFSky)
+        Me.GroupBox1.Controls.Add(Me.lblFWhite)
+        Me.GroupBox1.Controls.Add(Me.lblFBlack)
+        Me.GroupBox1.Controls.Add(Me.lblFBrown)
+        Me.GroupBox1.Controls.Add(Me.lblFViolet)
+        Me.GroupBox1.Controls.Add(Me.lblFYellow)
+        Me.GroupBox1.Controls.Add(Me.lblFGreen)
+        Me.GroupBox1.Controls.Add(Me.lblFOrange)
+        Me.GroupBox1.Controls.Add(Me.lblFBlue)
+        Me.GroupBox1.Controls.Add(Me.lblFRed)
+        Me.GroupBox1.Location = New System.Drawing.Point(115, 221)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(264, 123)
+        Me.GroupBox1.TabIndex = 20
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "ForeColor"
+        '
+        'lblFBlack
+        '
+        Me.lblFBlack.BackColor = System.Drawing.Color.Black
+        Me.lblFBlack.Location = New System.Drawing.Point(200, 26)
+        Me.lblFBlack.Name = "lblFBlack"
+        Me.lblFBlack.Size = New System.Drawing.Size(40, 32)
+        Me.lblFBlack.TabIndex = 0
+        '
+        'lblFBrown
+        '
+        Me.lblFBrown.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblFBrown.Location = New System.Drawing.Point(108, 67)
+        Me.lblFBrown.Name = "lblFBrown"
+        Me.lblFBrown.Size = New System.Drawing.Size(40, 32)
+        Me.lblFBrown.TabIndex = 0
+        '
+        'lblFViolet
+        '
+        Me.lblFViolet.BackColor = System.Drawing.Color.Fuchsia
+        Me.lblFViolet.Location = New System.Drawing.Point(154, 26)
+        Me.lblFViolet.Name = "lblFViolet"
+        Me.lblFViolet.Size = New System.Drawing.Size(40, 32)
+        Me.lblFViolet.TabIndex = 0
+        '
+        'lblFYellow
+        '
+        Me.lblFYellow.BackColor = System.Drawing.Color.Yellow
+        Me.lblFYellow.Location = New System.Drawing.Point(62, 67)
+        Me.lblFYellow.Name = "lblFYellow"
+        Me.lblFYellow.Size = New System.Drawing.Size(40, 32)
+        Me.lblFYellow.TabIndex = 0
+        '
+        'lblFGreen
+        '
+        Me.lblFGreen.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblFGreen.Location = New System.Drawing.Point(108, 26)
+        Me.lblFGreen.Name = "lblFGreen"
+        Me.lblFGreen.Size = New System.Drawing.Size(40, 32)
+        Me.lblFGreen.TabIndex = 0
+        '
+        'lblFOrange
+        '
+        Me.lblFOrange.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblFOrange.Location = New System.Drawing.Point(16, 67)
+        Me.lblFOrange.Name = "lblFOrange"
+        Me.lblFOrange.Size = New System.Drawing.Size(40, 32)
+        Me.lblFOrange.TabIndex = 0
+        '
+        'lblFBlue
+        '
+        Me.lblFBlue.BackColor = System.Drawing.Color.Blue
+        Me.lblFBlue.Location = New System.Drawing.Point(62, 26)
+        Me.lblFBlue.Name = "lblFBlue"
+        Me.lblFBlue.Size = New System.Drawing.Size(40, 32)
+        Me.lblFBlue.TabIndex = 0
+        '
+        'lblFRed
+        '
+        Me.lblFRed.BackColor = System.Drawing.Color.Red
+        Me.lblFRed.Location = New System.Drawing.Point(16, 26)
+        Me.lblFRed.Name = "lblFRed"
+        Me.lblFRed.Size = New System.Drawing.Size(40, 32)
+        Me.lblFRed.TabIndex = 0
+        '
+        'cboSelect
+        '
+        Me.cboSelect.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.cboSelect.FormattingEnabled = True
+        Me.cboSelect.Items.AddRange(New Object() {"12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32", "34", "36", "38", "40", "42", "44", "46", "48", "50"})
+        Me.cboSelect.Location = New System.Drawing.Point(455, 166)
+        Me.cboSelect.Name = "cboSelect"
+        Me.cboSelect.Size = New System.Drawing.Size(121, 28)
+        Me.cboSelect.TabIndex = 18
+        '
+        'btnLow
+        '
+        Me.btnLow.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnLow.Location = New System.Drawing.Point(536, 110)
+        Me.btnLow.Name = "btnLow"
+        Me.btnLow.Size = New System.Drawing.Size(40, 44)
+        Me.btnLow.TabIndex = 17
+        Me.btnLow.Text = "-"
+        Me.btnLow.UseVisualStyleBackColor = True
+        '
+        'btnHigh
+        '
+        Me.btnHigh.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnHigh.Location = New System.Drawing.Point(536, 65)
+        Me.btnHigh.Name = "btnHigh"
+        Me.btnHigh.Size = New System.Drawing.Size(40, 46)
+        Me.btnHigh.TabIndex = 16
+        Me.btnHigh.Text = "+"
+        Me.btnHigh.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.Location = New System.Drawing.Point(365, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 20)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "ขนาดฟอนต์"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(101, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 20)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "กำหนดตัวเลข"
+        '
+        'txtAdd
+        '
+        Me.txtAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtAdd.Location = New System.Drawing.Point(208, 66)
+        Me.txtAdd.Multiline = True
+        Me.txtAdd.Name = "txtAdd"
+        Me.txtAdd.Size = New System.Drawing.Size(328, 88)
+        Me.txtAdd.TabIndex = 13
+        '
+        'frmAssign3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(761, 408)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.cboSelect)
+        Me.Controls.Add(Me.btnLow)
+        Me.Controls.Add(Me.btnHigh)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtAdd)
+        Me.Name = "frmAssign3"
+        Me.Text = "frmColor"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblFSky As System.Windows.Forms.Label
+    Friend WithEvents lblFWhite As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblBSky As System.Windows.Forms.Label
+    Friend WithEvents lblBWhite As System.Windows.Forms.Label
+    Friend WithEvents lblBBlack As System.Windows.Forms.Label
+    Friend WithEvents lblBBrown As System.Windows.Forms.Label
+    Friend WithEvents lblBViolet As System.Windows.Forms.Label
+    Friend WithEvents lblBYellow As System.Windows.Forms.Label
+    Friend WithEvents lblBGreen As System.Windows.Forms.Label
+    Friend WithEvents lblBOrange As System.Windows.Forms.Label
+    Friend WithEvents lblBBlue As System.Windows.Forms.Label
+    Friend WithEvents lblBRed As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblFBlack As System.Windows.Forms.Label
+    Friend WithEvents lblFBrown As System.Windows.Forms.Label
+    Friend WithEvents lblFViolet As System.Windows.Forms.Label
+    Friend WithEvents lblFYellow As System.Windows.Forms.Label
+    Friend WithEvents lblFGreen As System.Windows.Forms.Label
+    Friend WithEvents lblFOrange As System.Windows.Forms.Label
+    Friend WithEvents lblFBlue As System.Windows.Forms.Label
+    Friend WithEvents lblFRed As System.Windows.Forms.Label
+    Friend WithEvents cboSelect As System.Windows.Forms.ComboBox
+    Friend WithEvents btnLow As System.Windows.Forms.Button
+    Friend WithEvents btnHigh As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtAdd As System.Windows.Forms.TextBox
+End Class
---
+++ VB_2N3_252-5/update frmAssign3/frmAssign3.vb
@@ -0,0 +1,96 @@
+﻿Public Class frmAssign3
+    Private Sub lblFRed_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFRed.Click
+        txtAdd.ForeColor = Color.Red
+    End Sub
+
+    Private Sub lblFBlue_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFBlue.Click
+        txtAdd.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub lblFGreen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFGreen.Click
+        txtAdd.ForeColor = Color.Green
+    End Sub
+
+    Private Sub lblFViolet_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFViolet.Click
+        txtAdd.ForeColor = Color.Violet
+    End Sub
+
+    Private Sub lblFBlack_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFBlack.Click
+        txtAdd.ForeColor = Color.Black
+    End Sub
+
+    Private Sub lblFOrange_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFOrange.Click
+        txtAdd.ForeColor = Color.Orange
+    End Sub
+
+    Private Sub lblFYellow_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFYellow.Click
+        txtAdd.ForeColor = Color.Yellow
+    End Sub
+
+    Private Sub lblFBrown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFBrown.Click
+        txtAdd.ForeColor = Color.Brown
+    End Sub
+
+    Private Sub lblFWhite_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFWhite.Click
+        txtAdd.ForeColor = Color.White
+    End Sub
+
+    Private Sub lblFSky_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFSky.Click
+        txtAdd.ForeColor = Color.SkyBlue
+    End Sub
+
+
+
+    Private Sub lblBRed_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBRed.Click
+        txtAdd.BackColor = Color.Red
+    End Sub
+
+    Private Sub lblBBlue_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBBlue.Click
+        txtAdd.BackColor = Color.Blue
+    End Sub
+
+    Private Sub lblBGreen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBGreen.Click
+        txtAdd.BackColor = Color.Green
+    End Sub
+
+    Private Sub lblBViolet_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBViolet.Click
+        txtAdd.BackColor = Color.Violet
+    End Sub
+
+    Private Sub lblBBlack_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBBlack.Click
+        txtAdd.BackColor = Color.Black
+    End Sub
+
+    Private Sub lblBOrange_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBOrange.Click
+        txtAdd.BackColor = Color.Orange
+    End Sub
+
+    Private Sub lblBYellow_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBYellow.Click
+        txtAdd.BackColor = Color.Yellow
+    End Sub
+
+    Private Sub lblBBrown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBBrown.Click
+        txtAdd.BackColor = Color.Brown
+    End Sub
+
+    Private Sub lblBWhite_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBWhite.Click
+        txtAdd.BackColor = Color.White
+    End Sub
+
+    Private Sub lblBSky_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBSky.Click
+        txtAdd.BackColor = Color.SkyBlue
+    End Sub
+
+    Private Sub btnUp_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHigh.Click
+        txtAdd.Text = Val(txtAdd.Text) + 3
+    End Sub
+
+    Private Sub btnDown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLow.Click
+        txtAdd.Text = Val(txtAdd.Text) - 3
+    End Sub
+
+    Private Sub cboSize_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboSelect.SelectedIndexChanged
+        txtAdd.Font = New Font(txtAdd.Font.Name, cboSelect.SelectedItem, FontStyle.Regular)
+    End Sub
+
+End Class
